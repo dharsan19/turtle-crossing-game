42dcee1a7d6ee9c3f7af2227dd224e770fd7a00e
Dharsan <dharsan19082002@gmail.com>
2025-10-02 10:31:28 +0530

    edited main.py

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 game_is_on = True
+
+player = Player()
+screen.listen()
+car = CarManager()
+
+screen.onkey(player.move_up, "Up")
+
 while game_is_on:
     time.sleep(0.1)
     screen.update()
